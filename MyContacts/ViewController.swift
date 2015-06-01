d8c199f0091ab9a69ddccdaa6f4790c66e2f33cb
Chuck Konkol <ckonkol@gmail.com>
2015-06-01 15:01:33 -0500

    update

--- MyContacts/ViewController.swift
+++ MyContacts/ViewController.swift
@@ -18,12 +18,19 @@ class ViewController: UIViewController {
         as! AppDelegate).managedObjectContext
     //2) Add variable contactdb (used from UITableView
     var contactdb:NSManagedObject!
-  
+
   
     @IBAction func btnBack(sender: AnyObject) {
         //3) Dismiss ViewController
         self.dismissViewControllerAnimated(false, completion: nil)
     }
+    
+    
+    @IBOutlet weak var lblfullname: UITextView!
+    
+    @IBOutlet weak var lblemail: UITextView!
+    
+    @IBOutlet weak var lblphone: UITextView!
     
     @IBOutlet weak var fullname: UITextField!
     
@@ -38,10 +45,27 @@ class ViewController: UIViewController {
         //4 Add Save Logic
         if (contactdb != nil)
         {
-            
+            if btnSave.currentTitle == "Edit"
+            {
+                btnSave.setTitle("Save", forState: UIControlState.Normal)
+                fullname.hidden = false
+                email.hidden = false
+                phone.hidden = false
+                lblfullname.hidden = true
+                lblemail.hidden = true
+                lblphone.hidden = true
+            }
+            else
+            {
             contactdb.setValue(fullname.text, forKey: "fullname")
             contactdb.setValue(email.text, forKey: "email")
             contactdb.setValue(phone.text, forKey: "phone")
+                var error: NSError?
+                managedObjectContext?.save(&error)
+                
+                self.dismissViewControllerAnimated(false, completion: nil)
+            }
+           
             
         }
         else
@@ -56,12 +80,14 @@ class ViewController: UIViewController {
             contact.fullname = fullname.text
             contact.email = email.text
             contact.phone = phone.text
+            var error: NSError?
+            managedObjectContext?.save(&error)
+            
+            self.dismissViewControllerAnimated(false, completion: nil)
         }
-        var error: NSError?
-        managedObjectContext?.save(&error)
+       
         
-        
-       self.dismissViewControllerAnimated(false, completion: nil)
+      
             
       
 
@@ -98,7 +124,17 @@ class ViewController: UIViewController {
             fullname.text = contactdb.valueForKey("fullname") as! String
             email.text = contactdb.valueForKey("email") as! String
             phone.text = contactdb.valueForKey("phone") as! String
-            btnSave.setTitle("Update", forState: UIControlState.Normal)
+            lblfullname.text = contactdb.valueForKey("fullname") as! String
+            lblemail.text = contactdb.valueForKey("email") as! String
+            lblphone.text = contactdb.valueForKey("phone") as! String
+            btnSave.setTitle("Edit", forState: UIControlState.Normal)
+          fullname.hidden = true
+             email.hidden = true
+             phone.hidden = true
+            lblfullname.hidden = false
+             lblemail.hidden = false
+             lblphone.hidden = false
+            
         }
         fullname.becomeFirstResponder()
         // Do any additional setup after loading the view.
